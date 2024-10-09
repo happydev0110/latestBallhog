549df704950658333a4ea20476d72b432106feac
Rick Slade <rickbslade@gmail.com>
2024-10-09 12:47:14 -0600

    Update func.js

--- src/func.js
+++ src/func.js
@@ -494,7 +494,7 @@ export const handleScore = (sportCategory, playItem, dataTypeItem, score, tableI
 
         // NFL DS Description
         case 'NFL-DS1':
-            description = 'Kickooffffff';
+            description = 'Kickoff';
             break;
         case 'NFL-DS1-1':
             description = 'Fumble Recovery';
