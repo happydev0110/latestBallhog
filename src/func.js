96776ba96b045ccfbe8305ef8acd66658c10ce1d
Rick Slade <rickbslade@gmail.com>
2024-10-17 20:57:21 -0600

    Update func.js

--- src/func.js
+++ src/func.js
@@ -280,6 +280,26 @@ export const handleScore = (sportCategory, playItem, dataTypeItem, score, tableI
                 description = 'Blocked shot by '
             }
             break;
+        case 'NHL-DS17':
+            if (playItem.participants) {
+                description = 'Penalty. ' + playItem.participants[0].athlete.shortName
+            } else {
+                description = 'Penalty. '
+            }
+            break;
+        case 'NHL-DS17-1':
+            if (playItem.participants) {
+                description = 'Penalty. ' + playItem.participants[0].athlete.shortName
+            } else {
+                description = 'Penalty. '
+            }
+            break;
+        case 'NHL-DS17-2':
+            description = 'Stoppage. Penalties on both teams.'
+            break;
+        case 'NHL-DS17-3':
+            description = 'Stoppage. Penalties on both teams'
+            break;
 
         // NHL2 
         case 'NHL2-DS1':
